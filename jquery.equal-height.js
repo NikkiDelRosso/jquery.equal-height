ffa1a16ca5aeb94e7384e0d65faffd153b14523a
Nikki DelRosso <nikk3r+github@gmail.com>
2017-08-22 17:11:26 -0700

    Update heights on page fully loaded

--- jquery.equal-height.js
+++ jquery.equal-height.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Equal Height v0.0.1 (https://github.com/Nikker/jquery.equal-height)
+ * jQuery Equal Height v0.0.2 (https://github.com/Nikker/jquery.equal-height)
  * Copyright 2015-2016 Nikki DelRosso
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
@@ -43,4 +43,5 @@ jQuery.fn.equalHeight = function() {
 	setHeights();
 	$(window).resize(setHeights);
 	$(this).find('img').load(setHeights);
+	$(window).bind('load', setHeights);
 };
